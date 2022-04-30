23a404702ded192dd03826659311f44b3e647a70
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-04-30 15:48:13 -0500

    Se creo la clase VisualpartnerService y el metodo getAllVisualpartners

--- test/services/Visualpartner_service.test.js
+++ test/services/Visualpartner_service.test.js
@@ -1,11 +1,15 @@
 
 const VisualpartnerService = require("./../../lib/services/Visualpartner_service");
+const fs = require("fs");
+const rawdata = fs.readFileSync("./test/data_test/visualpartner_testdata.json");
+const visualpartner_testdata = JSON.parse(rawdata);
 
 describe("Suite de tests para la clase VisualpartnerService", () => {
 
     it("1) Probando el metodo getAllVisualpartners retorne todos los valores", () => {
 
-        const visualpartners = VisualpartnerService.getAllVisualpartners();
+        const visualpartners = VisualpartnerService.getAllVisualpartners(visualpartner_testdata);
         expect(visualpartners).not.toBeUndefined();
+        expect(visualpartners.length).toBe(2);
     })
 })
