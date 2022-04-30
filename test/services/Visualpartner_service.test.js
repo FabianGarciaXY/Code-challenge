7656d298f5fdeca73ffa03f0769090a9818df5b5
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-04-30 17:31:21 -0500

    Se modifico el archivo Readme.md

--- test/services/Visualpartner_service.test.js
+++ test/services/Visualpartner_service.test.js
@@ -5,9 +5,12 @@ const rawdata = fs.readFileSync("./test/data_test/visualpartner_testdata.json");
 const visualpartner_testdata = JSON.parse(rawdata);
 
 describe("Suite de tests para la clase VisualpartnerService", () => {
+
     it("1) Probando el metodo getAllVisualpartners retorne todos los valores", () => {
         const visualpartners = VisualpartnerService.getAllVisualpartners(visualpartner_testdata);
         expect(visualpartners).not.toBeUndefined();
         expect(visualpartners.length).toBe(2);
     });
+
+    
 });
