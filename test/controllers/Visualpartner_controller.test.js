ed4f36de5257cfa1c1807948d2f7aad359127f30
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-04-30 19:11:52 -0500

    Se creo la prueba unitaria para getStudentsWithCertification de controllers y se hizo fallar

--- test/controllers/Visualpartner_controller.test.js
+++ test/controllers/Visualpartner_controller.test.js
@@ -1,8 +1,14 @@
 const VisualpartnerController = require("../../lib/controllers/Visualpartner_controller");
 
 describe("Set de tests para el controllador VisualpartnerController", () => {
+
     test("1) Validanto el metodo estatico getAllVisualpartners acepte los datos del archivo JSON", () => {
         const visualparnerData = VisualpartnerController.getAllVisualpartners();
         expect(visualparnerData).not.toBeUndefined();
     });
+
+    test("2) Validando que el metodo getStudentsWithCertification del cotrolador retorne so la lista de estudiantes que tengan certificación", () => {
+        const visualparnerData = VisualpartnerController.getStudentsWithCertification();
+        expect(visualparnerData).not.toBeUndefined();
+    })
 });
