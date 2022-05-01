c01c3602c6db3c793c5e07262b02b022bc56c2f0
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-04-30 19:05:35 -0500

    Se creo el metodo getStudentsWithCertification de la clase service y se pasaron las pruebas

--- test/services/Visualpartner_service.test.js
+++ test/services/Visualpartner_service.test.js
@@ -9,11 +9,13 @@ describe("Suite de tests para la clase VisualpartnerService", () => {
     it("1) Probando el metodo getAllVisualpartners retorne todos los valores", () => {
         const visualpartners = VisualpartnerService.getAllVisualpartners(visualpartner_testdata);
         expect(visualpartners).not.toBeUndefined();
-        expect(visualpartners.length).toBe(2);
+        expect(visualpartners.length).toBe(5);
     });
 
     it("2) Validando el metodo static getStudentsWithCertification", () => {
         const visualpartners = VisualpartnerService.getStudentsWithCertification(visualpartner_testdata);
+        console.log(visualpartners)
+
         expect(visualpartners).not.toBeUndefined();
         expect(visualpartners.length).toBe(3);
     })
